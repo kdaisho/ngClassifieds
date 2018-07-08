5759efcbf659f2974a2af68bc1b0c6b293c84550
Daisho <daishokomiyama@gmail.com>
2018-07-08 12:21:17 -0400

    Adding saveClassified function and mdToast notification

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,11 +4,17 @@
 
     angular
         .module('ngClassifieds')
-        .controller('classifiedsCtrl', function($scope, classifiedsFactory, $mdSidenav) {
+        .controller('classifiedsCtrl', function($scope, classifiedsFactory, $mdSidenav, $mdToast) {
 
             classifiedsFactory.getClassifieds().then(function(res) {
                 $scope.classifieds = res.data;
             });
+
+            var contact = {
+                name: 'Daisho',
+                phone: '(555) 524-5588',
+                email: 'daishokomiyama@gmail.com'
+            };
 
             $scope.openSidebar = function() {
                 $mdSidenav('left').open();
@@ -16,6 +22,22 @@
 
             $scope.closeSidebar = function() {
                 $mdSidenav('left').close();
+            }
+
+            $scope.saveClassified = function(classified) {
+                if (classified) {
+                    classified.contact = contact;
+                    $scope.classifieds.push(classified)
+                    $scope.classified = {};
+                    $scope.closeSidebar();
+
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .content('Classified saved!')
+                            .position('top, right')
+                            .hideDelay(3000)
+                    );
+                }
             }
         });
 })();
